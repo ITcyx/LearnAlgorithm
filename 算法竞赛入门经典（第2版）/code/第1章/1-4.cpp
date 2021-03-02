26d4bae03a8ea955b4a5b858e55f3ca960af3f48
Andy Cai <1781953879@qq.com>
2021-03-02 11:47:05 +0800

    add 算法竞赛入门经典（第2版） code 1-4.cpp

---
+++ 算法竞赛入门经典（第2版）/code/第1章/1-4.cpp
@@ -0,0 +1,11 @@
+﻿#define _USE_MATH_DEFINES
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	printf("%.3f %.3f\n", sin(n / 180.0 * M_PI), cos(n / 180.0 * M_PI));
+	return 0;
+}
