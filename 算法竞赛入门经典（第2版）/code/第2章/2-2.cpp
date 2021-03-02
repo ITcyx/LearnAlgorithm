1f217182c518b58d426760bf41c3139d87f0afb1
Andy Cai <1781953879@qq.com>
2021-03-02 15:42:44 +0800

    add 算法竞赛入门经典（第2版） code 2-2.cpp

---
+++ 算法竞赛入门经典（第2版）/code/第2章/2-2.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int i = 0, a, b, c, n;
+	while (scanf("%d%d%d", &a, &b, &c) == 3)
+	{
+		printf("Case %d: ", ++i);
+		n = (70 * a + 21 * b + 15 * c) % 105;
+		if (n < 10 || n>100)
+			printf("No answer\n");
+		else
+			printf("%d\n", n);
+	}
+	return 0;
+}
