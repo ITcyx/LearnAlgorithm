68bdde7431243ee8881e8eeb2c92863dd2c6dd15
Andy Cai <1781953879@qq.com>
2021-03-02 16:11:33 +0800

    add 算法竞赛入门经典（第2版） code 2-4.cpp

---
+++ 算法竞赛入门经典（第2版）/code/第2章/2-4.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int n, m, c = 0;
+	long long i;
+	double a;
+	while (scanf("%d%d", &n, &m) == 2)
+	{
+		if (n == 0 && m == 0)
+			break;
+		a = 0;
+		for (i = n; i <= m; ++i)
+			a += 1.0 / (i*i);
+		printf("Case %d: %.5f\n", ++c, a);
+	}
+	return 0;
+}
