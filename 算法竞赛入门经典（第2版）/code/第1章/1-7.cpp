b5d3715ed30379afdd7e95682cd3c88af3eaee1e
Andy Cai <1781953879@qq.com>
2021-03-02 13:56:53 +0800

    add 算法竞赛入门经典（第2版） code 1-7.cpp

---
+++ 算法竞赛入门经典（第2版）/code/第1章/1-7.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	if (n % 400 == 0)
+		printf("yes\n");
+	else if (n % 100 == 0)
+		printf("no\n");
+	else if (n % 4 == 0)
+		printf("yes\n");
+	else
+		printf("no\n");
+	return 0;
+}
