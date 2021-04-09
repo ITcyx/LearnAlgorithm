8ae4e8d6b14b2cb8b00c6b6ae7aaeabcbbd0162c
Andy Cai <1781953879@qq.com>
2021-04-09 11:33:09 +0800

    add LeetCode code 程序员面试金典（第6版） 02.05.cpp

--- LeetCode/code/程序员面试金典（第 6 版）/02.05.cpp
+++ LeetCode/code/程序员面试金典（第 6 版）/02.05.cpp
@@ -6,53 +6,53 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution 
+class Solution
 {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
-    {
-        ListNode* s=new ListNode;
-        ListNode* h=s;
-        ListNode* p1=l1;
-        ListNode* p2=l2;
-        int c=0,t;
-        while(p1&&p2)
-        {
-            t=p1->val+p2->val+c;
-            s->next=new ListNode(t%10);
-            c=t/10;
-            s=s->next;
-            p1=p1->next;
-            p2=p2->next;
-        }
-        while(p1)
-        {
-            if(c)
-            {
-                t=p1->val+c;
-                s->next=new ListNode(t%10);
-                c=t/10;
-            }
-            else
-                s->next =new ListNode(p1->val);
-            s=s->next;
-            p1=p1->next;
-        }
-        while(p2)
-        {
-            if(c)
-            {
-                t=p2->val+c;
-                s->next=new ListNode(t%10);
-                c=t/10;
-            }
-            else
-                s->next =new ListNode(p2->val);
-            s=s->next;
-            p2=p2->next;
-        }
-        if(c)
-            s->next=new ListNode(c);
-        return h->next;
-    }
+	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
+	{
+		ListNode* s = new ListNode;
+		ListNode* h = s;
+		ListNode* p1 = l1;
+		ListNode* p2 = l2;
+		int c = 0, t;
+		while (p1&&p2)
+		{
+			t = p1->val + p2->val + c;
+			s->next = new ListNode(t % 10);
+			c = t / 10;
+			s = s->next;
+			p1 = p1->next;
+			p2 = p2->next;
+		}
+		while (p1)
+		{
+			if (c)
+			{
+				t = p1->val + c;
+				s->next = new ListNode(t % 10);
+				c = t / 10;
+			}
+			else
+				s->next = new ListNode(p1->val);
+			s = s->next;
+			p1 = p1->next;
+		}
+		while (p2)
+		{
+			if (c)
+			{
+				t = p2->val + c;
+				s->next = new ListNode(t % 10);
+				c = t / 10;
+			}
+			else
+				s->next = new ListNode(p2->val);
+			s = s->next;
+			p2 = p2->next;
+		}
+		if (c)
+			s->next = new ListNode(c);
+		return h->next;
+	}
 };
