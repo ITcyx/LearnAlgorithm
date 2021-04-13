9720ea4edbd4d9a52de98e9d80ef9555759b570e
Andy Cai <1781953879@qq.com>
2021-04-13 22:43:13 +0800

    add 算法竞赛经典入门（第2版） code 第3章 例题 3-2.cpp

---
+++ 算法竞赛入门经典（第2版）/code/第3章/例题/3-2.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	char t1[] = "1234567890-=WERTYUIOP[]\\SDFGHJKL;'XCVBNM,./";
+	char t2[] = "`1234567890-QWERTYUIOP[]ASDFGHJKL;ZXCVBNM,.";
+	char table[128], c;
+	int i;
+	for (i = 0; i < 128; ++i)
+		table[i] = i;
+	for (i = 0; i < 44; ++i)
+		table[t1[i]] = t2[i];
+	while ((c = getchar()) != EOF)
+		printf("%c", table[c]);
+	return 0;
+}
