24c06bbc5e750728081cf2c4c4ebd810f22aa004
Andy Cai <1781953879@qq.com>
2021-04-16 17:16:59 +0800

    add 算法竞赛入门经典（第2版） code 第3章 习题 3-1.cpp

---
+++ 算法竞赛入门经典（第2版）/code/第3章/习题/3-1.cpp
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+
+int main()
+{
+	int T, i, j = 0, p = 0;
+	char s[80];
+	scanf("%d", &T);
+	while (T--)
+	{
+		scanf("%s", s);
+		j = 0, p = 0;
+		for (i = 0; s[i]; ++i)
+			if (s[i] == 'O')
+				p += ++j;
+			else
+				j = 0;
+		printf("%d\n", p);
+	}
+	return 0;
+}
