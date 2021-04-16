ab2300431949fd0988f04da0b2e6e7e02bd5ec5f
Andy Cai <1781953879@qq.com>
2021-04-16 15:28:56 +0800

    add 算法竞赛入门经典（第2版） code 第3章 例题 3-5.cpp

---
+++ 算法竞赛入门经典（第2版）/code/第3章/例题/3-5.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int table[100050], i, n, t;
+	for (i = 0; i < 100050; ++i)
+		table[i] = 0;
+	for (i = 1; i < 100000; ++i)
+	{
+		n = t = i;
+		while (t > 0)
+		{
+			n += t % 10;
+			t /= 10;
+		}
+		if (table[n] == 0)
+			table[n] = i;
+		else if (table[n] > i)
+			table[n] = i;
+	}
+	for (scanf("%d", &i); i; --i)
+	{
+		scanf("%d", &n);
+		printf("%d\n", table[n]);
+	}
+	return 0;
+}
