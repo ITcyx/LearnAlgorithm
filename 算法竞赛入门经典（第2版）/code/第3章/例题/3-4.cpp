8e1bb4c70fd5d03da6bf765721288d2ca89317c4
Andy Cai <1781953879@qq.com>
2021-04-16 14:45:08 +0800

    add 算法竞赛入门经典（第2版） code 第3章 例题 3-4.cpp

---
+++ 算法竞赛入门经典（第2版）/code/第3章/例题/3-4.cpp
@@ -0,0 +1,46 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int answer[1010], guess[1010], table_a[10], table_g[10];
+	int len, game_count = 0, i, t, A, B;
+	while (scanf("%d", &len) == 1 && len)
+	{
+		printf("Game %d:\n", ++game_count);
+		for (i = 1; i < 10; ++i)
+			table_a[i] = 0;
+		for (i = 0; i < len; ++i)
+		{
+			scanf("%d", &t);
+			answer[i] = t;
+			++table_a[t];
+		}
+		while (true)
+		{
+			for (i = 1; i < 10; ++i)
+				table_g[i] = 0;
+			for (i = 0; i < len; ++i)
+			{
+				scanf("%d", &t);
+				guess[i] = t;
+				++table_g[t];
+			}
+			if (guess[0] == 0)
+				break;
+			A = B = 0;
+			for (i = 1; i < 10; ++i)
+				if (table_a[i] < table_g[i])
+					table_g[i] = table_a[i];
+			for(i=0;i<len;++i)
+				if (answer[i] == guess[i])
+				{
+					--table_g[answer[i]];
+					++A;
+				}
+			for (i = 1; i < 10; ++i)
+				B += table_a[i] < table_g[i] ? table_a[i] : table_g[i];
+			printf("    (%d,%d)\n", A, B);
+		}
+	}
+	return 0;
+}
